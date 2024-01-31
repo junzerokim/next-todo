129cd5e4e358fe73adaecd6b1e61b8270a56a418
junyoung <junyoung@junyoungs-MacBook-Pro.local>
2024-01-31 23:07:38 +0900

    :dizzy: Add: Todo Input UI

--- components/todos-table.tsx
+++ components/todos-table.tsx
@@ -1,10 +1,44 @@
 'use client';
 
-import React from 'react';
-import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from '@nextui-org/react';
+import React, { useState } from 'react';
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  Input,
+  Button,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from '@nextui-org/react';
 import { Todo } from '@/types';
 
 const TodosTable = ({ todos }: { todos: Todo[] }) => {
+  // 할 일 추가 가능 여부
+  const [todoAddEnable, setTodoAddEnable] = useState(false);
+
+  // 입력된 할 일
+  const [newTodoInput, setNewTodoInput] = useState('');
+
+  const disabledTodoAddButton = () => {
+    return (
+      <Popover placement="top">
+        <PopoverTrigger className="h-14">
+          <Button>Add</Button>
+        </PopoverTrigger>
+        <PopoverContent>
+          <div className="px-1 py-2">
+            <div className="text-small font-bold">🫥</div>
+            <div className="text-tiny">Please enter your task</div>
+          </div>
+        </PopoverContent>
+      </Popover>
+    );
+  };
+
   const TodoRow = (todo: Todo) => {
     return (
       <TableRow key={todo.id}>
@@ -16,20 +50,40 @@ const TodosTable = ({ todos }: { todos: Todo[] }) => {
     );
   };
   return (
-    <Table aria-label="Example static collection table">
-      <TableHeader>
-        <TableColumn>ID</TableColumn>
-        <TableColumn>TODO</TableColumn>
-        <TableColumn>STATUS</TableColumn>
-        <TableColumn>TIME</TableColumn>
-      </TableHeader>
-      <TableBody emptyContent={'No rows to display.'}>
-        {todos &&
-          todos.map((todo: Todo) => {
-            return TodoRow(todo);
-          })}
-      </TableBody>
-    </Table>
+    <>
+      <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+        <Input
+          type="text"
+          label="To Do"
+          value={newTodoInput}
+          onValueChange={(changedInput) => {
+            setNewTodoInput(changedInput);
+            setTodoAddEnable(changedInput.length > 0);
+          }}
+        />
+        {todoAddEnable ? (
+          <Button color="secondary" className="h-14">
+            Add
+          </Button>
+        ) : (
+          disabledTodoAddButton()
+        )}
+      </div>
+      <Table aria-label="Example static collection table">
+        <TableHeader>
+          <TableColumn>ID</TableColumn>
+          <TableColumn>TODO</TableColumn>
+          <TableColumn>STATUS</TableColumn>
+          <TableColumn>TIME</TableColumn>
+        </TableHeader>
+        <TableBody emptyContent={'No rows to display.'}>
+          {todos &&
+            todos.map((todo: Todo) => {
+              return TodoRow(todo);
+            })}
+        </TableBody>
+      </Table>
+    </>
   );
 };
 
